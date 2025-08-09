ed8839960f09370f2bba2816ecb5d5b488ec72f5
bipin-saha <bipinsaha142@gmail.com>
2025-08-09 23:38:21 +0600

    deploy v4 beta

---
+++ src/app/(pages)/research/Add.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Form from "@/app/(pages)/research/Form";
+import Modal from "@/components/Modal";
+import { useUserAuth } from "@/context/userAuth";
+import { databases } from "@/lib/appwrite";
+import { getImgBBUrl } from "@/lib/imgbb";
+import { FormEvent } from "react";
+import { toast } from "react-toastify";
+
+const AddButton = () => {
+  const { user } = useUserAuth();
+  const handleAdd = async (event: FormEvent) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget as HTMLFormElement);
+    let thumbnail: string | null | undefined = null;
+    const thumbnailInput = formData.get("thumbnail") as File | null;
+    const modal = document.getElementById("Add-Research");
+    if (modal) {
+      (modal as HTMLDialogElement).close(); // Close the modal after update
+    }
+    if (thumbnailInput && thumbnailInput.size > 0) {
+      thumbnail = await getImgBBUrl([thumbnailInput]);
+    }
+    const newData: {
+      title: FormDataEntryValue;
+      content: FormDataEntryValue;
+      ref: FormDataEntryValue | null;
+      code: FormDataEntryValue | null;
+      time: FormDataEntryValue;
+      thumbnail?: string | null | undefined;
+    } = {
+      title: formData.get("title") ?? "",
+      content: formData.get("content") ?? "",
+      ref: formData.get("ref") || null,
+      code: formData.get("code") || null,
+      time: formData.get("time") || new Date().toISOString(),
+    };
+
+    // Only update thumbnail if a new one was uploaded
+    if (thumbnail) {
+      newData.thumbnail = thumbnail;
+    }
+    try {
+      await databases.createDocument(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_APPWRITE_RESEARCH!,
+        "unique()",
+        newData
+      );
+      toast.info("Successfully added information");
+    } catch (error) {
+      toast.error("Failed to add information");
+      console.error(error);
+    }
+
+    // Reload the page
+    location.reload();
+  };
+
+  if (!user?.$id) {
+    return null; // Don't show the button if the user is not authenticated
+  }
+  return (
+    <Modal id="Add-Research" title="Add Research">
+      <form onSubmit={handleAdd} className="flex flex-col w-full gap-3">
+        <Form />
+        <button className="btn btn-info my-5" type="submit">
+          Add
+        </button>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddButton;
